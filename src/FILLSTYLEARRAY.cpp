826a5f52376a7e40b4bdc030a32a270bf316def7
Yukariin <yukariin@yandex.ru>
2015-06-18 14:49:07 +0300

    Add FILLSTYLEARRAY base class

---
+++ src/FILLSTYLEARRAY.cpp
@@ -0,0 +1,1 @@
+#include "FILLSTYLEARRAY.h"
---
+++ src/FILLSTYLEARRAY.h
@@ -0,0 +1,13 @@
+#ifndef LIBSWF_FILLSTYLEARRAY_H
+#define LIBSWF_FILLSTYLEARRAY_H
+
+
+#include <vector>
+
+class FILLSTYLEARRAY {
+public:
+	vector<int> fillStyles;
+};
+
+
+#endif //LIBSWF_FILLSTYLEARRAY_H
